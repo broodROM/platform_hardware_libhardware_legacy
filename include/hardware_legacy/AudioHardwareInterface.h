1bca08cc88295b2af39ee0d95ac6de9273aaeef0
broodplank <guichardflorent@gmail.com>
2013-12-30 06:50:56 +0100

    Revert "libhardware_legacy: Clean up all the audio ifdefs"
    This reverts commit 41f1ad76ef5e8bdaeb28b68af8b2a034131560f8.

--- include/hardware_legacy/AudioHardwareInterface.h
+++ include/hardware_legacy/AudioHardwareInterface.h
@@ -118,7 +118,7 @@ public:
      */
 #ifndef ICS_AUDIO_BLOB
     virtual status_t    getNextWriteTimestamp(int64_t *timestamp);
-#ifdef QCOM_DIRECTTRACK
+#ifdef QCOM_HARDWARE
     virtual status_t    start() {return INVALID_OPERATION;}
     virtual status_t    pause()  {return INVALID_OPERATION;}
     virtual status_t    flush()  {return INVALID_OPERATION;}
